e1be7f886dbecd73da62bcb459386472121aeeda
Jen Clarke <39578135+JeClarke05@users.noreply.github.com>
2018-09-05 16:25:39 -0700

    fixed resource file and updated master.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,8 @@
 #import the os and CSV Python modules
 import os
 import csv
+import datetime
+import time
 
 #create os path to csv resource file
 csvpath = os.path.join("", "Resources", "budget_data.csv")
@@ -23,37 +25,51 @@ with open(csvpath, 'r', newline="", encoding='utf-8') as csvfile:
     
     #Skip header of CSV file
     csv_header = next(csvreader)
-    #Loop through csv data
+    #Loop through csv data to pull info from CSV file and organize in to variables and lists
     for row in csvreader:
-        #add date to list and count number of items in list to get number of months
         monthYear.append(row[0])
         num_months = len(monthYear)
         rev.append(row[1])
-        #calculate total net 
+        #calculate total net by adding the contents of the profit/losses column
         totalNet = totalNet + int(row[1])
     
-
+#loop though the revenue list to calculate the Month over Month change and append to new list
 for x in range(1, len(rev)):
         diff = (int(rev[x])) - int(rev[x-1])
         momDifList.append(diff)
-        
+
+#find max value in the month over month differences for the greatest increase in prophet      
 Gincrease = max(momDifList)
 Gdecrease = min(momDifList)
 
+#use the index of the greatest increase and decrease value to find the coresponding month
 indexGincrease = momDifList.index(Gincrease)
 indexGdecrease = momDifList.index(Gdecrease)
 
 GincreaseMon = monthYear[indexGincrease + 1]
 GdecreaseMon = monthYear[indexGdecrease + 1]
 
+
+#use the total of the difference list divided by the length of the difference list to get the average change
 aveChange = sum(momDifList) / len(momDifList)   
 
 #Print Financial Analysis Header 
 print("Financial Analysis")
 print("------------------------------") 
-#Print total months to the summary output 
+#Print summary output 
 print("Total Months: " + str(num_months))
 print("Total: $" + str(totalNet))
-print("Average Change: " + str(round(aveChange, 2)))
-print("Greatest Increase in Profits: " + GincreaseMon + "(" + str(Gincrease) + ")")
-print("Greatest Decrease in Profits: " + GdecreaseMon + "(" + str(Gdecrease) + ")")
+print("Average Change: $" + str(round(aveChange, 2)))
+print("Greatest Increase in Profits: " + GincreaseMon + "($" + str(Gincrease) + ")")
+print("Greatest Decrease in Profits: " + GdecreaseMon + "($" + str(Gdecrease) + ")")
+
+#output summary to txt file 
+text_file = open("Output.txt", "w")
+text_file.write(f'Financial Analysis\n')
+text_file.write(f'------------------------------\n')
+text_file.write(f'Total Months: {str(num_months)}\n')
+text_file.write(f'Total: $ {str(totalNet)}\n')
+text_file.write(f'Average Change: $ {str(round(aveChange, 2))}\n')
+text_file.write(f'Greatest Increase in Profits: {GincreaseMon} (${str(Gincrease)})\n')
+text_file.write(f'Greatest Decrease in Profits: {GdecreaseMon} (${str(Gdecrease)})\n')
+text_file.close()
